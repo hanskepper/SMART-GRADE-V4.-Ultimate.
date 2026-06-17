179fcf9c0513a0f347402c9caf4a8f56391538ef
hanskepper <hanskepper52@gmail.com>
2026-06-17 10:44:30 +0100

    Add files via upload
    Signed-off-by: hanskepper <hanskepper52@gmail.com>

---
+++ maintenance-config.js
@@ -0,0 +1,8 @@
+// ============================================
+// MAINTENANCE MODE CONFIGURATION
+// ============================================
+// Mettez true pour activer la maintenance
+// Mettez false pour désactiver la maintenance
+// ============================================
+
+var MAINTENANCE_MODE = true; // false = désactivé (par défaut)

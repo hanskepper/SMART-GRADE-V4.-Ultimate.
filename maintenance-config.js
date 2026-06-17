43739e8b7c221309e5e0f5e0b87b3435d5bde99c
hanskepper <hanskepper52@gmail.com>
2026-06-17 10:47:41 +0100

    Update maintenance-config.js

--- maintenance-config.js
+++ maintenance-config.js
@@ -5,4 +5,4 @@
 // Mettez false pour désactiver la maintenance
 // ============================================
 
-var MAINTENANCE_MODE = true; // false = désactivé (par défaut)
+var MAINTENANCE_MODE = flase; // false = désactivé (par défaut)
